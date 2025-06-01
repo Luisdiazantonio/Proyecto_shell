fbb340afa1ba692ac3f01198bd48a2744307a154
JoseLuisBello <110626222+JoseLuisBello@users.noreply.github.com>
2025-06-01 11:56:23 -0600

    Update MiniShell_Cliente.c

--- MiniShell_Cliente.c
+++ MiniShell_Cliente.c
@@ -229,7 +229,7 @@ void conexion() {
 	struct sockaddr_in server_addr;
 	memset(&server_addr, 0, sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(8080);
+	server_addr.sin_port = htons(1666);
 	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
 
 	if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
