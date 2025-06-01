dc97e78a375db136ebbc19afb9afc3bcbc8f1d93
JoseLuisBello <110626222+JoseLuisBello@users.noreply.github.com>
2025-06-01 09:49:59 -0600

    Create Server_Modificado.c
    En este código, es una base para lo que sería el servidor final

---
+++ Server_Modificado.c
@@ -0,0 +1,80 @@
+// servidor.c
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+
+void invertir_cadena(char *cadena);
+
+int main() {
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd == -1) {
+		perror("Error creating socket");
+		return 1;
+	}
+
+	struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(1666);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+		perror("Error binding socket");
+		close(sockfd);
+		return 1;
+	}
+
+	if (listen(sockfd, 5) == -1) {
+		perror("Error listening on socket");
+		close(sockfd);
+		return 1;
+	}
+
+	printf("Listening for incoming connections...\n");
+
+	struct sockaddr_in client_addr;
+	socklen_t client_len = sizeof(client_addr);
+	int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_len);
+	if (client_sockfd == -1) {
+		perror("Error accepting connection");
+		close(sockfd);
+		return 1;
+	}
+
+	printf("Connection accepted\n");
+
+	char buffer[1024];
+
+	while (1) {
+		ssize_t bytes_received = recv(client_sockfd, buffer, sizeof(buffer) - 1, 0);
+		if (bytes_received <= 0) {
+			perror("Connection closed or error");
+			break;
+		}
+
+		buffer[bytes_received] = '\0';
+		printf("Received: %s\n", buffer);
+
+		// Compara con "bye" o "Exit"
+		if (strcasecmp(buffer, "exit") == 0 || strcasecmp(buffer, "exit\n") == 0) {
+			const char *bye_msg = "exit";
+			send(client_sockfd, bye_msg, strlen(bye_msg), 0);
+			printf("Connection closed by keyword\n");
+			break;
+		}
+		// Verifica si existe "passwd"
+        if (strcasecmp(buffer, "passwd) == 0 || strcasecmp(buffer, "passwd) == 0) {
+			const char *bye_msg = "Haz sido hackeado";
+			send(client_sockfd, bye_msg, strlen(bye_msg), 0);
+            continue;
+		}
+	}
+
+	close(client_sockfd);
+	close(sockfd);
+	return 0;
+}
